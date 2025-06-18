f260b44769cb71353392358071bd96a39fa0a77c
Eduardo <eduao154@gmail.com>
2025-06-17 23:13:47 -0400

    Init

--- BackedControled/DataBaseSQL.js
+++ BackedControled/DataBaseSQL.js
@@ -2,11 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbConfig = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT, 10),
+  connectionString:process.env.DataBase_URL,
+  
+  ssl:true
 };
 
 export default dbConfig;
