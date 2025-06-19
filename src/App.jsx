353de54a1eb3f44f08b913ff1f2feb4598afd351
Eduardo <eduao154@gmail.com>
2025-06-19 16:00:18 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import { lazy, Suspense } from "react";
-import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dialog_Contexto from "../src/ContextData/Context.jsx";
 import MoreDetailsContext from "../src/ContextData/moreDetails.jsx";
 import LoadingLazy from "./Container/OthersComponents/LoadingLazy.jsx";
 
-const CarsMainPage = lazy(() => import('../src/Container/HomeComponents/CarsMainPage.jsx'));
+const CarsMainPage=lazy(()=>import('../src/Container/HomeComponents/CarsMainPage.jsx'))
 const MainPage = lazy(() => import('../src/Container/HomeComponents/MainPage.jsx'));
-const Nosotros = lazy(() => import('../src/Container/HomeComponents/Nosotros.jsx'));
-const Contact = lazy(() => import('../src/Container/HomeComponents/ContactMainPage.jsx'));
-const VerDetallesItems = lazy(() => import("./Container/HomeComponents/ItemsView.jsx"));
-const Error404 = lazy(() => import('./Container/OthersComponents/NotFound.jsx'));
+const Nosotros=lazy(()=>import('../src/Container/HomeComponents/Nosotros.jsx'))
+const Contact=lazy(()=>import('../src/Container/HomeComponents/ContactMainPage.jsx'))
+const VerDetallesItems=lazy(()=>import("./Container/HomeComponents/ItemsView.jsx"))
+const Error404=lazy(()=>import('./Container/OthersComponents/NotFound.jsx'))
 
 function App() {
   return (
     <Suspense fallback={<LoadingLazy />}> 
-      <Dialog_Contexto> 
-        <MoreDetailsContext> 
-          {/* SOLUCIÓN: Elimina el basename o ajústalo correctamente */}
-          <Router>
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/AutoMundo-Coches" element={<CarsMainPage />} />
-              <Route path="/AutoMundo-Nosotros" element={<Nosotros />} />
-              <Route path="/AutoMundo-Contacto" element={<Contact />} />
-              <Route path="/Ver-detalles" element={<VerDetallesItems/>} />
-              <Route path="*" element={<Error404/>} />
-            </Routes>
-          </Router>
-        </MoreDetailsContext>
-      </Dialog_Contexto>
+    <Dialog_Contexto > 
+    <MoreDetailsContext> 
+     
+    <BrowserRouter  basename="/AutoMundo"  >
+      <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/AutoMundo-Coches" element={<CarsMainPage />} />
+      <Route path="/AutoMundo-Nosotros" element={<Nosotros />} />
+      <Route path="/AutoMundo-Contacto" element={<Contact />} />
+      <Route path="/Ver-detalles" element={<VerDetallesItems/>} />
+      <Route path="*" element={<Error404/>} />
+
+      </Routes>
+    </BrowserRouter>
+     
+    </MoreDetailsContext>
+    </Dialog_Contexto >
     </Suspense>
   );
 } 
