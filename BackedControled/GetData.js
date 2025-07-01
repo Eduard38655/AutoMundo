ffcb5e53426433e3478bfc7322e4ba1fbc381f1f
Eduardo <eduao154@gmail.com>
2025-06-30 23:04:48 -0400

    Update GetData.js

--- BackedControled/GetData.js
+++ BackedControled/GetData.js
@@ -1,37 +1,41 @@
-// routes/GetData.js
-import { Router } from 'express';
-import pkg from 'pg';
-import dbConfig from '../DataBaseSQL.js';
+import express from "express";
+import pkg from "pg";
+import dbConfig from "./DataBaseSQL.js";
 const { Pool } = pkg;
-const pool = new Pool(dbConfig);
 
-const router = Router();
-
-router.get('/', async (req, res) => {
+const router = express.Router();
+const pool = new Pool(dbConfig); // usa el config correcto
+/*Routes Data */
+router.get("/GetData", async (req, res) => {
   try {
-    const result = await pool.query(`
-      SELECT 
-        a.autoid,
-        a.tipoauto,
-        a.precio,
-        a.status,
-        a.description,
-        a.yearauto AS yearmarca,
-        a.img,
-        m.marcas,
-        c.carac_motor,
-        c.carac_interior,
-        c.carac_velocidad,
-        c.carac_sistemas
-      FROM autos a
-      JOIN marcas m ON a.marcas_id = m.marcas_id
-      JOIN caracteristicas c ON a.carac_id = c.carac_id;
-    `);
+   const result = await pool.query(`
+  SELECT 
+    a.autoid,
+    a.tipoauto,
+    a.precio,
+    a.status,
+    a.description,
+    a.yearauto,
+    a.img,
+    a.typeauto,
+    m.marcas,
+    m.yearmarca,
+    c.carac_motor,
+    c.carac_interior,
+    c.carac_velocidad,
+    c.carac_sistemas
+  FROM autos a
+  INNER JOIN marcas m ON a.marcas_id = m.marcas_id
+  INNER JOIN caracteristicas c ON a.carac_id = c.carac_id;
+`);
+
 
     res.status(200).json({ data: result.rows });
+    
   } catch (err) {
-    console.error('Error retrieving data:', err);
-    res.status(500).json({ message: 'Error retrieving data' });
+    
+    console.error("Error retrieving data:", err);
+    res.status(500).json({ message: "Error retrieving data" });
   }
 });
 
